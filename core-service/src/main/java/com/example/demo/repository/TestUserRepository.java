548a431eca35c2b5a8c852426ab5d478a9749c18
Hvndn <dienhovan2000@gmail.com>
2026-05-19 23:28:15 +0700

    Clean: Remove all database test portal mock classes and restore original clean frontend layout

--- core-service/src/main/java/com/example/demo/repository/TestUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.TestUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestUserRepository extends JpaRepository<TestUser, Long> {
-}
